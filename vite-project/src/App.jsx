5c413c3123e355c0259c0874df08fb8f37029ab4
Mausam Kumari <mausamkumari9095@gmail.com>
2025-11-20 23:43:35 +0530

    some changes

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,10 @@
+import Morning from './first'
+
+function App() {
+  
+  return (
+    <Morning/>
+  );
+}
+
+export default App
---
+++ vite-project/src/first.jsx
@@ -0,0 +1,6 @@
+function Morning(){
+    return(
+        <h3>sunday morning</h3>
+    )
+}
+export default Morning
